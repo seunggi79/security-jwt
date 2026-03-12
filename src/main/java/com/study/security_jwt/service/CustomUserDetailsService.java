631ac9bf518c31ee757c8819927ca1f4fb3b5144
seunggi <hsg77689469@gmail.com>
2026-03-12 19:53:00 +0900

    세션 정보

--- src/main/java/com/study/security_jwt/service/CustomUserDetailsService.java
+++ src/main/java/com/study/security_jwt/service/CustomUserDetailsService.java
@@ -23,12 +23,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         //DB에서 조회
         UserEntity userData = userRepository.findByUsername(username);
 
-        if (userData != null) {
+        if (userData == null) {
 
             //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
-            return new CustomUserDetails(userData);
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
         }
 
-        return null;
+        return new CustomUserDetails(userData);
     }
 }
